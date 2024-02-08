4142eb016a3b939e47a4307bb735617333da3f94
Data Analyst <abusufian.tex.cu@gmail.com>
2024-02-08 21:56:36 +0000

    Update app_stremlit.py

--- app_stremlit.py
+++ app_stremlit.py
@@ -7,7 +7,7 @@ from keras.models import load_model
 import streamlit as st
 from PIL import Image
 
-st.image("BrainTumor.gif", use_column_width=True)
+st.image("BrainTumor.gif", width=300)
 
 # Title and description
 st.title("4D AI Driven Neuro App")
