03843e45a7016b1bea154b9de9a8ac400b2efa76
Data Analyst <abusufian.tex.cu@gmail.com>
2024-02-10 02:30:43 +0000

    Update app_stremlit.py

--- app_stremlit.py
+++ app_stremlit.py
@@ -7,22 +7,16 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 st.image("TAC_Brain_tumor_glioblastoma-Transverse_plane.gif", use_column_width=True)
-
-# Sidebar for file upload
 st.sidebar.title("Brain Tumor Detection")
 st.sidebar.write("User friendly, Public can test the MRI image segmentation accuracy")
 uploaded_image = st.sidebar.file_uploader("Choose an MRI image...", type=["jpg", "jpeg", "png"], key="file_uploader")
 uploaded_mask = st.sidebar.file_uploader("Choose the corresponding ground truth mask...", type=["jpg", "jpeg", "png"], key="mask_uploader")
-
-# Title and description
 st.title("4D AI Driven Neuro App")
 st.write("This app demonstrates Brain Image segmentation and tumor detection and prevention using a web application.")
 st.markdown("<span style='color:blue'>Author Md Abu Sufian</span>", unsafe_allow_html=True)
 st.write("......Visualisation of Design and Coding Under Construction.........")
+MODEL_PATH = 'BrainTumor10Epochs.h5'
 
-MODEL_PATH = 'BrainTumor10Epochs.h5'  # The name of the model file
-
-# Function to download the model if it's not already on the filesystem
 def download_model(model_url, model_path):
     if not os.path.isfile(model_path):
         with st.spinner('Downloading model...'):
@@ -33,42 +27,33 @@ def download_model(model_url, model_path):
             else:
                 raise Exception(f"Error downloading the model: HTTP {response.status_code}")
 
-# Load your trained model
 @st.cache(allow_output_mutation=True)
 def load_my_model():
     model_url = 'https://raw.githubusercontent.com/datascintist-abusufian/Neuro-App-AI-driven-4D-brain-image-processing-on-standalone-platforms/main/' + MODEL_PATH
     download_model(model_url, MODEL_PATH)
-    model = load_model(MODEL_PATH)  # Load the model from the local path
+    model = load_model(MODEL_PATH)
     return model
 
 model = load_my_model()
 
 if uploaded_image is not None and uploaded_mask is not None:
-    img = Image.open(uploaded_image).convert('RGB').resize((64, 64))  # Resize the image before processing
-    mask = Image.open(uploaded_mask).convert('L').resize((64, 64))  # Resize the mask before processing
+    img = Image.open(uploaded_image).convert('RGB').resize((64, 64))
+    mask = Image.open(uploaded_mask).convert('L').resize((64, 64))
     st.image(img, caption="Uploaded MRI Image", use_column_width=True)
-
     try:
         img_array = np.array(img) / 255.0
         img_array = np.expand_dims(img_array, axis=0)
-        
         pred_mask = model.predict(img_array)
-        
         threshold = 0.5
         binary_mask = (pred_mask > threshold).astype(np.uint8)
-        
-        # Create an RGBA image for the overlay with the mask as the alpha channel
         mask_colored = np.stack([binary_mask*0, binary_mask*255, binary_mask*0, binary_mask*255], axis=-1)
         overlay = Image.fromarray(mask_colored, mode='RGBA')
         img_with_overlay = Image.alpha_composite(img.convert('RGBA'), overlay)
-        
         st.image(img_with_overlay, caption="Segmentation Result", use_column_width=True)
-
-        # Calculate and display the accuracy
-        mask_array = np.array(mask)  # The mask is already the correct size
+        mask_array = np.array(mask)
         accuracy = accuracy_score(mask_array.flatten(), binary_mask.flatten())
         st.write(f"Prediction accuracy: {accuracy:.2f}")
     except Exception as e:
-        st.error(f"An error occurred: {str(e)}")  # Display the actual error message
+        st.error(f"An error occurred: {str(e)}")
 else:
     st.error("Please upload an image file and its corresponding ground truth mask.")
